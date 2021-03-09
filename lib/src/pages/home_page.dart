e5402ba8d6dd1c25b16500864eed4e486c8d5dbf
Antonio Arencibia <arencibiacasa@gmail.com>
2021-03-09 18:46:46 +0000

    Añadimos un slider

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,8 +16,33 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      body: SafeArea(
-        child: Text('Hola mundo.........'),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _swiperTarjetas() {
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network(
+            "https://www.elagoradiario.com/wp-content/uploads/2019/04/m%C3%B3jate1.2.jpg",
+            fit: BoxFit.fill,
+          );
+        },
+        itemCount: 3,
+        // pagination: SwiperPagination(),
+        // control: SwiperControl(),
       ),
     );
   }
